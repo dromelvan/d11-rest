7877352e6f148e3a3642d4d5a3e216c9e9a2822a
dromelvan <dromelvan@aland.net>
2020-08-06 19:35:50 +0300

    Exact player name search by using quotation marks.

--- src/main/java/org/d11/rest/repository/PlayerRepository.java
+++ src/main/java/org/d11/rest/repository/PlayerRepository.java
@@ -24,5 +24,20 @@ public interface PlayerRepository extends D11RestRepository<Player> {
                    "ORDER BY name",
             nativeQuery = true)
     public List<PlayerSearchResult> findByParameterizedNameLike(@Param("parameterizedName")String parameterizedName);
-        
+
+    @Query(value = "SELECT ps.id, TRIM(FROM concat(first_name, ' ', last_name)) AS name, ts.id AS teamId, ts.name AS teamName " + 
+                   "FROM players ps " + 
+                   "     JOIN player_season_infos psis ON ps.id = psis.player_id " + 
+                   "     JOIN teams ts ON ts.id = psis.team_id " + 
+                   "     WHERE season_id = (SELECT MAX(id) FROM seasons) " + 
+                   "     AND parameterized_name = :parameterizedName " + 
+                   "UNION " + 
+                   "SELECT ps.id, TRIM(FROM concat(first_name, ' ', last_name)) AS name, 1 AS teamId, 'None' AS teamName " + 
+                   "FROM players ps " + 
+                   "     WHERE parameterized_name = :parameterizedName " + 
+                   "     AND NOT EXISTS(SELECT id FROM player_season_infos psis WHERE player_id = ps.id AND season_id = (SELECT MAX(id) FROM seasons)) " + 
+                   "ORDER BY name",
+            nativeQuery = true)
+    public List<PlayerSearchResult> findByParameterizedName(@Param("parameterizedName")String parameterizedName);
+    
 }
--- src/main/java/org/d11/rest/service/SearchService.java
+++ src/main/java/org/d11/rest/service/SearchService.java
@@ -1,6 +1,7 @@
 package org.d11.rest.service;
 
 import java.util.List;
+import java.util.regex.*;
 import java.util.stream.Collectors;
 
 import org.d11.rest.api.model.*;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SearchService extends D11RestService {
 
+    private Pattern exactSearchPattern = Pattern.compile("\"(.*)\"");
     private PlayerRepository playerRepository;
     private final static Logger logger = LoggerFactory.getLogger(SearchService.class);
     
@@ -23,14 +25,22 @@ public class SearchService extends D11RestService {
     }
     
     public SearchResultDTO search(String search) {
-        StringBuilder stringBuilder = new StringBuilder("%");
-        for(String string : search.split("[ +]")) {
-            stringBuilder.append(Parameterizer.parameterize(string));
-            stringBuilder.append("%");
-        }        
-        logger.trace("Performing search with search string {}.", stringBuilder);
+        List<PlayerSearchResult> players = null;
         
-        List<PlayerSearchResult> players = this.playerRepository.findByParameterizedNameLike(stringBuilder.toString());
+        Matcher exactPatternMatcher = this.exactSearchPattern.matcher(search);        
+        if(exactPatternMatcher.matches()) {
+            search = Parameterizer.parameterize(search);            
+            logger.trace("Performing search with search string {}.", search);
+            players = this.playerRepository.findByParameterizedName(search);
+        } else {
+            StringBuilder stringBuilder = new StringBuilder("%");
+            for(String string : search.split("[ +]")) {
+                stringBuilder.append(Parameterizer.parameterize(string));
+                stringBuilder.append("%");
+            }        
+            logger.trace("Performing search with search string {}.", stringBuilder);
+            players = this.playerRepository.findByParameterizedNameLike(stringBuilder.toString());
+        }
         
         SearchResultDTO searchResultDTO = new SearchResultDTO();
         searchResultDTO.getPlayers().addAll(players.stream().map(player -> map(player, PlayerSearchResultDTO.class)).collect(Collectors.toList()));        
